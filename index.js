d47d7dd7bfbdace245bfa9a25802aa49e8c0df27
Sajmul <sajmul1427@gmail.com>
2024-12-13 23:49:38 +0600

    job application post api created

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.saftd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,19 +21,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const jobCollection = client.db('jobPortal').collection('jobs');
+    const jobCollection = client.db("jobPortal").collection("jobs");
+    const jobApplicationCollection = client
+      .db("jobPortal")
+      .collection("applications");
 
-    app.get('/jobs', async(req, res) => {
+    app.get("/jobs", async (req, res) => {
       const result = await jobCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/jobs/:id', async(req, res) => {
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await jobCollection.findOne(query);
       res.send(result);
-    })
+    });
+
+    // application apis
+
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
 
     await client.connect();
 
@@ -48,7 +58,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Job server is running");
